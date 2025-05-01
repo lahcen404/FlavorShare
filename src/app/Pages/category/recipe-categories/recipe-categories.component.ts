93516c42accb093dc5330be15e6074af8fcc0205
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-01 15:15:35 +0100

    create Categories section

---
+++ src/app/Pages/category/recipe-categories/recipe-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-categories.component.html',
+  styleUrl: './recipe-categories.component.css'
+})
+export class RecipeCategoriesComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar/navbar.component";
 import { HomeComponent } from "./Pages/home/home/home.component";
 import { FooterComponent } from "./shared/footer/footer/footer.component";
+import { RecipeCategoriesComponent } from "./Pages/category/recipe-categories/recipe-categories.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, RecipeCategoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
